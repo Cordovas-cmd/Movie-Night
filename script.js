b7a4c4b3ab1316ae6561379eed8be1ca3227fa26
Samuel Cordova <cordovas1337@gmail.com>
2022-11-16 00:01:39 -0600

    added get by rating and event listener

--- script.js
+++ script.js
@@ -54,3 +54,26 @@ function showMovies(movies) {
     })
 }
 
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
