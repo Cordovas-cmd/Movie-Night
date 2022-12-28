3cbf501b50533f2384406b08677c436cc3004484
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2022-12-27 22:56:22 -0600

    updates

--- script.js
+++ script.js
@@ -12,6 +12,8 @@
 
 // might change apis./
 
+// DOn't want to put my info in for an api will keep looking
+
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
